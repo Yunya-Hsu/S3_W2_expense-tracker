a22ab8f0449bddf70e4724b573e414bfab336baa
Yunya Hsu <yunyahsu110@gmail.com>
2022-06-20 14:25:00 +0800

    feat: add edit feature

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 // 引入外部套件
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const port = process.env.PORT
 const router = require('./routers/index')
@@ -20,6 +21,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true })) // 設定body-parser(解析post傳回來的req，body-parser已包在express中)
+app.use(methodOverride('_method'))  // 設定每一筆請求都會透過method-override進行前置處理
 
 // 啟動路由
 app.use(router)
--- routers/modules/expense.js
+++ routers/modules/expense.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const expense = require('../../models/expense')
 const router = express.Router()
 
 const Expense = require('../../models/expense')
@@ -10,7 +11,42 @@ router.get('/new', (req, res) => {
 router.post('/new', (req, res) => {
   const newExpense = req.body
   newExpense.userId = 'test123'
+  
+  if (!newExpense.categoryId) {
+    res.render('new', { newExpense })
+  }
+
   Expense.create(newExpense)
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const _id = req.params.id
+  Expense.findOne({ _id })
+    .lean()
+    .then(result => {
+      res.render('edit', { result })
+    })
+    .catch(err => console.error(err))
+})
+
+router.put('/:id', (req, res) => {
+  const result = req.body
+  const _id = req.params.id
+  result._id = _id
+
+  if (!result.categoryId) {
+    res.render('edit', { result })
+  }
+
+  Expense.findOne({ _id })
+    .then(theExpense => {
+      for (const key of Object.keys(result)) {
+        theExpense[key] = result[key]
+      }
+      return theExpense.save()
+    })
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
 })
